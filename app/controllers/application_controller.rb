36a81f12a1859bbbaa210cbdc2a028315eabba6a
Blaine Richardson <blainerrichardson@gmail.com>
2017-10-05 23:17:57 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,30 +11,30 @@ class ApplicationController < Sinatra::Base
   end
 
   post '/login' do
-    @user = User.find_by(username: params["username"], password: params["password"])
-    if @user == nil
-      erb :error
-    else
+    @user = User.find_by(username: params[:username])
+    if @user
       session[:user_id] = @user.id
-      redirect '/account'
+      redirect to :account
+    else
+      erb :error
     end
   end
 
   get '/account' do
-    if session[:user_id] == nil
-      erb :error
-    else
+    @current_user = User.find_by_id(session[:user_id])
+    if @current_user
       erb :account
+    else
+      erb :error
     end
   end
 
   get '/logout' do
     session.clear
-    redirect '/'
+    redirect to '/'
   end
 
   get '/error' do
     erb :error
   end
-
 end
